14e4d508deb7d0b971ebb3ee8b5ab7bd9baf6ccd
CynthiaShiro <aimydewlux@gmail.com>
2020-09-15 16:51:37 +0300

    initial comit

--- vat.py
+++ vat.py
@@ -1,21 +1,21 @@
 def getTotal(amount, sum, VAT):
-    balance = sum - (amount * VAT)
+    balance = ((amount * VAT) + amount) - sum
     return balance
 
    
 def userInput():
    
     amount = int(input(" Enter the total amount to be paid: "))  
-    print("Total is: KES {0:.2f} ".format(amount))  
+    print("Total is: KES {0:,.2f} ".format(amount))  
 
     sum = int(input(" Enter the amount paid:"))
-    print("amount is: KES {0:.2f} ".format(sum))
+    print("amount is: KES {0:,.2f} ".format(sum))
     vat = float(input(" Enter the TAX rate:"))
     print("VAT is: KES {0:.2f} ".format(vat))
 
     balance = getTotal(amount, sum, vat)
 
-    print("Your balance is: KES {0:.2f} ".format(balance))
+    print("Your balance is: KES {0:,.2f} ".format(balance))
     print(balance)
 
 
